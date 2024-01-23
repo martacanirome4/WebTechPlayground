40aff9a2c0e3d8d6fd3a05d108209b265164c4f5
MartaCanino <martacaninoromero@gmail.com>
2024-01-23 17:59:06 +0100

    ejemplo de express generator

--- nodejs/http/http_post.js
+++ nodejs/http/http_post.js
@@ -5,6 +5,13 @@
 /* making a simple HTTP POST request to the '/todos' endpoint on a local server running on 'localhost:3000'
 It sends a message ('Mensaje') in the request body and logs the response status code and data to the console. */
 
+/*  POST
+• Equivalente al GET
+• Hay que añadir cabeceras
+• Hay que añadir el mensaje
+• PUT/DELETE serían equivalentes al POST
+ */
+
 const http = require('http');
 
 const data = "Mensaje";
